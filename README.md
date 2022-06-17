3cc0835ae09bfe96275480a67ccd54de61ba2a3d
Najib Ishaq <najib_ishaq@zoho.com>
2022-06-17 13:54:28 -0400

    wip: added basics for publishing to crates.io

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # z5
+
+Rust bindings for the [zarr](https://github.com/zarr-developers/zarr-python) and [n5](https://github.com/saalfeldlab/n5) file formats.
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod n5;
+pub mod zarr;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
